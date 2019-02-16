47d6a98d15dd2fec9a22bdcfe938c751bb698aeb
Waldinian <waldinian@Alexs-MacBook-Pro.local>
2019-02-16 14:51:44 -0500

    adding a colorbar, because QGIS crashes when colorcoding by timestamp.

--- csvParser.py
+++ csvParser.py
@@ -52,14 +52,15 @@ df = pd.DataFrame()
 df['time'] = t
 df['lat'] = lat
 df['lon'] = lon
+df['colorbar'] = [(1000*i)//len(t) for i in range(len(t))]
 print(df)
 
 directory = '{}/FilteredLocationsChunked_{}'.format(args.Timestamp, args.Timestamp)
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-df.to_csv('{}FilteredLocationsFull_{}.csv'.format(args.Timestamp,args.Timestamp))
-df[::len(df)//5000].to_csv('{}FilteredLocations5k_{}.csv'.format(args.Timestamp,args.Timestamp))
-df[::len(df)//15000].to_csv('{}FilteredLocations15k_{}.csv'.format(args.Timestamp,args.Timestamp))
+df.to_csv('{}/FilteredLocationsFull_{}.csv'.format(args.Timestamp,args.Timestamp))
+df[::len(df)//5000].to_csv('{}/FilteredLocations5k_{}.csv'.format(args.Timestamp,args.Timestamp))
+df[::len(df)//15000].to_csv('{}/FilteredLocations15k_{}.csv'.format(args.Timestamp,args.Timestamp))
 for i in range(32):
 	df[i*len(df)//32:(i+1)*len(df)//32].to_csv('{}/FilteredLocationsChunked_{}/FilteredLocation{}of32_{}.csv'.format(args.Timestamp,args.Timestamp, (i+1), args.Timestamp))
