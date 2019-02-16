d3095bd62c5dae48409ebfbd56bd25ab3277b7b3
Alex Fox <waldinian@Alexs-MBP.fios-router.home>
2019-02-15 20:36:08 -0500

    implementing decimal fix

--- csvParser.py
+++ csvParser.py
@@ -16,50 +16,44 @@ d = dfile.readlines()
 
 lat = d[2::5]
 lon = d[3::5]
-time = d[4::5]
+t = d[4::5]
 
 def getVal(data):
 	for i in range(len(data)):
 
 		val = ''
 		number = False
-		for j in range(len(data[i])):
+
+		j = 0
+		while True:
 
 			if data[i][j] == ',':
 
-				print('4')
-				data[i] = float(val) * 10 **-7
+				data[i] = float(val) * 10**-7
 				break
 			
 			while not number:
 
-				print('5')
-				print(data[i][j])
-				k += 1
+				j += 1
 				if data[i][j] == ':':
 
-					print('6')
 					j += 1
 					number = True
-					print('help I\'m stuck')
 
-			print('7')
-			print('nevermind I\'m out')
-			
 			val += data[i][j]
+			j += 1
 
 getVal(lat)
 getVal(lon)
-for i in range(len(time)):
-	time[i] = float(time[i][20:33]*10**-3)
+for i in range(len(t)):
+	t[i] = float(t[i][20:33]) * 10**-3
 
 df = pd.DataFrame()
-df['time'] = time
+df['time'] = t
 df['lat'] = lat
 df['lon'] = lon
 print(df)
 
-'''
 directory = '{}/FilteredLocationsChunked_{}'.format(args.Timestamp, args.Timestamp)
 if not os.path.exists(directory):
     os.makedirs(directory)
@@ -69,5 +63,3 @@ df[::len(df)//5000].to_csv('{}FilteredLocations5k_{}.csv'.format(args.Timestamp,
 df[::len(df)//15000].to_csv('{}FilteredLocations15k_{}.csv'.format(args.Timestamp,args.Timestamp))
 for i in range(32):
 	df[i*len(df)//32:(i+1)*len(df)//32].to_csv('{}/FilteredLocationsChunked_{}/FilteredLocation{}of32_{}.csv'.format(args.Timestamp,args.Timestamp, (i+1), args.Timestamp))
-
-	'''
