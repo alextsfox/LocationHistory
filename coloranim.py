11994ec76d8a06b12a17ad46912d5c22528507b8
Waldinian <waldinian@wireless-10-104-50-5.umd.edu>
2019-02-17 15:31:09 -0500

    working animation with colorbar, but the colorbar doesnt have more than one color for some reason

--- coloranim.py
+++ coloranim.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 
 # load file in ascending time order, load (n x 1) arrays of lat, lon, and colorbar
 locHist = np.loadtxt(args.fileIn, delimiter = ',', skiprows=1)[::-1]
-colorData = np.array([(x, 0, 1-x) for x in locHist[:,0]])
+colorData = locHist[:,0]
 lat = locHist[:,2]
 lon = locHist[:,3]
 
@@ -18,13 +18,15 @@ numFrames = len(colorData)
 
 # creating figure object
 fig = plt.figure()
-scat = plt.scatter([],[], s=10)#, c=[], cmap='viridis')
+
+# for some reason, when we give it an empty array, it spazzes out, so we give it empty slices.
+scat = plt.scatter(lon[:1],lat[:1], c=colorData[:1], s=10, cmap='viridis')
 
 # initial frame function is an empty colorbar and empty point array
 def init():
 
 	scat.set_offsets([])
-	#scat.set_array([])
+	scat.set_array([])
 
 	return scat,
 
@@ -32,46 +34,21 @@ def init():
 def animate(i, X=lon, Y=lat, T=colorData):
 
 	# set_offsets needs an nx2 array, so we use hstack to rotate and stack our arrays.
-	#print(len(X))
 	XY = np.hstack((X[:i,np.newaxis], Y[:i, np.newaxis]))
-	print(XY)
-	print(XY.shape)
 	scat.set_offsets(XY)
-	#scat.set_array(T)
+	# who knows why we need this?
+	scat.set_array(np.ravel(T[:i]))
 
 	return scat,
 
 # frame speed, ms
-interval = 10
+interval = 0
 
 #print(interval)
 plt.xlim(-100,-65)
 plt.ylim(35,45)
+print('starting animation')
 animation = anim.FuncAnimation(fig, animate, init_func=init,frames=numFrames, interval=interval, blit=True,)
-
-plt.show()
-
-
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import matplotlib.animation as animation
-
-# def main():
-#     numframes = 100
-#     numpoints = 10
-#     color_data = np.random.random((numframes, numpoints))
-#     x, y, c = np.random.random((3, numpoints))
-
-#     fig = plt.figure()
-#     scat = plt.scatter(x, y, c=c, s=100)
-
-#     ani = animation.FuncAnimation(fig, update_plot, frames=xrange(numframes),
-#                                   fargs=(color_data, scat))
-#     plt.show()
-
-# def update_plot(i, data, scat):
-#     scat.set_array(data[i])
-#     return scat,
-
-# main()
+print('saving...')
+animation.save('animtest.mp4', fps=500, extra_args=['-vcodec', 'libx264'])
+print('saved')
