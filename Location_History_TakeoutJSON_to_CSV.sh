61062b6cef6fc04b5dcbc234dfeb7f99ebaece4b
Waldinian <waldinian@wireless-10-104-50-5.umd.edu>
2019-02-17 18:42:05 -0500

    completing the color animation tool

--- Location_History_TakeoutJSON_to_CSV.sh
+++ Location_History_TakeoutJSON_to_CSV.sh
@@ -4,6 +4,14 @@ timestamp=02142019
 RawJSON=$timestamp/location_history_$timestamp.json
 filteredJSON=$timestamp/filtered_locations_timestamped_$timestamp.json
 
+echo 'Filtering location history archive...'
+
 `cat $RawJSON |jq "[.locations[] | {latitudeE7, longitudeE7, timestampMs}]" > $filteredJSON`
 
+echo 'Converting archive to csv...'
+
 python3 csvParser.py $filteredJSON $timestamp
+
+echo 'Making your movie, this may take a while...'
+
+python3 coloranim.py $timestamp/FilteredLocationsFull_$timestamp.csv $timestamp/MyTravels_$timestamp.mp4
--- coloranim.py
+++ coloranim.py
@@ -1,10 +1,12 @@
 #takes a csv file as input, outputs a movie.
+# this script is likely extremeluy volatile, and very innefficient. Only use on the output up csvParser.py or Location_History_TakeoutJSON_to_CSV.sh
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import numpy as np
 import argparse
 from datetime import datetime
+import sys
 
 # get csv file
 parser = argparse.ArgumentParser()
@@ -25,10 +27,10 @@ lat = locHist[:,2]
 lon = locHist[:,3]
 
 # creating figure object
-fig = plt.figure(figsize=(20,10))
+fig = plt.figure(figsize=(30,15))
 
 # for some reason, when we give it an empty array, it spazzes out, so we give it empty slices.
-scat = plt.scatter(lon[:1],lat[:1], c=colorData[:1], s=0.4)
+scat = plt.scatter(lon[:1],lat[:1], c=colorData[:1], s=2, cmap='viridis_r')
 
 #colorbar and axis limits
 plt.xlim(-100,-65)
@@ -37,27 +39,61 @@ plt.clim(colorData[0], colorData[-1])
 plt.axis('off')
 
 #print('Ideal Timestep: ', (colorData[-1] - colorData[0])/3600)
-#The number of points per frame by timestamp so that in the end we have a 60 second movie
+#The number of points per frame by timestamp so that in the end we have a ~120 second movie
+
+# a pretty progress bar
+def update_progress(progress):
+    barLength = 63 # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\r[{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), round(progress*100,2), status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+# time to advance each frame
 def getPtsThisFrame(i):
-
-	# time to advance this frame
-	timestep = (colorData[-1] - colorData[0])/3600
-
-
+	
+	timestep = (colorData[-1] - colorData[0])/10800
 	# count frames ahead until we see that the timestep is below the threshold
 	indexStep = 0#i
 	while timestep > 0:
-		try:
-			timestep -= (colorData[i + indexStep+1] - colorData[i + indexStep])
-			indexStep += 1
-		except IndexError as err:
-			break
-
+		timestep -= (colorData[i + indexStep+1] - colorData[i + indexStep])
+		indexStep += 1
 	#print(i,len(colorData), 'IndexStep',indexStep, 'TrueTimeStep', colorData[i+indexStep]-colorData[i], 'TimeStepRemainder',timestep)
 
 	nextIndex = i+indexStep
 	# return the proper number of frames to advance to
 	return nextIndex
+
+k=0
+l = 0
+
+# calculate the number of frames we'll need
+def mockAnimate(i, X=lon, Y=lat, T=colorData):
+	global k
+	global l
+	
+	try:
+		global k
+		nextIndex = getPtsThisFrame(k)
+		k = nextIndex
+		l += 1
+		# set_offsets needs an nx2 array, so we use hstack to rotate and stack our arrays.
+	
+	# sneaky when we've run out the end of our data, trigger the save and exit condition.
+	except IndexError as err:
+		return l	
 
 # initial frame function is an empty colorbar and empty point array
 def init():
@@ -67,25 +103,41 @@ def init():
 
 	return scat,
 
-# animation function progressively adds points by plotting slices [:i] iteratively.
+j=0
+done=False
+
+for m in range(len(colorData)):
+	if mockAnimate(m) is not None:
+		numFrames = mockAnimate(m)
+
 def animate(i, X=lon, Y=lat, T=colorData):
 
-	nextIndex = getPtsThisFrame(i)
+	
+	try:
+		global j
+		nextIndex = getPtsThisFrame(j)
+		j = nextIndex
 
-	# set_offsets needs an nx2 array, so we use hstack to rotate and stack our arrays.
-	XY = np.hstack((X[:nextIndex,np.newaxis], Y[:nextIndex, np.newaxis]))
-	scat.set_offsets(XY)
-	# who knows why we need this?
-	scat.set_array(np.ravel(T[:nextIndex]))
+		# set_offsets needs an nx2 array, so we use hstack to rotate and stack our arrays.
+		XY = np.hstack((X[:nextIndex,np.newaxis], Y[:nextIndex, np.newaxis]))
+		scat.set_offsets(XY)
+		# who knows why we need this?
+		scat.set_array(np.ravel(T[:nextIndex]))
+
+		update_progress(i/numFrames)
+	
+	# sneaky when we've run out the end of our data, trigger the save and exit condition.
+	except IndexError as err:
+		pass
+		#rint('stuck at index', i)
 
 	return scat,
 
 # frame speed, ms
 interval = 0
-numFrames = len(colorData)
 
 print('starting animation...')
 animation = anim.FuncAnimation(fig, animate, init_func=init,frames=numFrames, interval=interval, blit=True)#True)
-print('saving...')
+print('animating...')
 animation.save(args.fileOut, fps=500, extra_args=['-vcodec', 'libx264'])
-print('saved')
+print('successfully saved as', args.fileOut)
