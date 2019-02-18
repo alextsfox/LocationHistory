54de276ac7a009531867e7f6adbed7c9f7d31026
Waldinian <waldinian@Alexs-MBP.fios-router.home>
2019-02-17 23:49:16 -0500

    wrong file references

--- Location_History_TakeoutJSON_to_CSV.sh
+++ Location_History_TakeoutJSON_to_CSV.sh
@@ -20,7 +20,7 @@ python3 csvParser.py $filteredJSON $dir
 #select the resoltuion (choose between Ultra Low Res: '05k', Low Res: '15k', and Full Res: 'Full')
 Resolution=Full
 
-python3 coloranim.py $dir/FilteredLocations$Resolution.csv $dir/MyTravels_$Resolution.png
+python3 colorImage.py $dir/FilteredLocations$Resolution.csv $dir/MyTravels_$Resolution.png
 
 echo 'Making your movie, this will take a long time...'
 
