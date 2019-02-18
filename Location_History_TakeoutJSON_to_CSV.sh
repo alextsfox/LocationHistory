a32d6342932dc57411bdeb0dda367376b7a95aa6
Waldinian <waldinian@Alexs-MBP.fios-router.home>
2019-02-17 23:51:21 -0500

    correct directory

--- Location_History_TakeoutJSON_to_CSV.sh
+++ Location_History_TakeoutJSON_to_CSV.sh
@@ -20,8 +20,8 @@ python3 csvParser.py $filteredJSON $dir
 #select the resoltuion (choose between Ultra Low Res: '05k', Low Res: '15k', and Full Res: 'Full')
 Resolution=Full
 
-python3 colorImage.py $dir/FilteredLocations$Resolution.csv $dir/MyTravels_$Resolution.png
+python3 colorImage.py $dir/FilteredLocations$Resolution.csv $dir/figs/MyTravels_$Resolution.png
 
 echo 'Making your movie, this will take a long time...'
 
-python3 coloranim.py $dir/FilteredLocations$Resolution.csv $dir/MyTravels_$Resolution.mp4
+python3 coloranim.py $dir/FilteredLocations$Resolution.csv $dir/figs/MyTravels_$Resolution.mp4
