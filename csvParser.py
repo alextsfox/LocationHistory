317a2fdee3c4cf610e94fd8b80a7d9df074f920f
Waldinian <waldinian@Alexs-MBP.fios-router.home>
2019-02-22 11:43:12 -0500

    starting to create a custom slicer

--- csvParser.py
+++ csvParser.py
@@ -1,21 +1,26 @@
-import argparse
 import os
+import sys
+import argparse
+from time import time
 import pandas as pd
+import numpy as np
+
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('JSON_In', help='The filtered .json file')
-parser.add_argument('CSV_Out_Dir', help='CSV out filename')
+parser.add_argument('jsonIn', help='The filtered .json file')
+parser.add_argument('-s', '--slice', type=int, nargs=2, help='<start timestamp> <end timestamp> in seconds. Use this option if you only want to convert use a subset of the data.')
 
 args = parser.parse_args()
 
-dfile = open(args.JSON_In, 'r')
+dfile = open(args.jsonIn, 'r')
 d = dfile.readlines()
 
 lat = d[2::5]
 lon = d[3::5]
 t = d[4::5]
 
+# converts JSON data into organized lists.
 def getVal(data):
 	for i in range(len(data)):
 
@@ -46,14 +51,59 @@ getVal(lon)
 for i in range(len(t)):
 	t[i] = float(t[i][20:33]) * 10**-3
 
+# creating dataframe
 df = pd.DataFrame()
 df['time'] = t
 df['lat'] = lat
 df['lon'] = lon
 df['colorbar'] = [(1000*i)//len(t) for i in range(len(t))]
+
+csvOut = '/'.join(args.jsonIn.split('/')[:-1]) + '/FilteredLocations'
+
+
+if args.slice is not None: # user specified a custom slice
+
+	if args.slice[0] > 2*time() and args.slice[1] > 2*time():
+		print('You entered a timeslice in the future. Aborting.')
+		sys.exit()
+
+	print('Creating custom files...')
+
+	timeIndexA = 1
+	timeIndexB = 1
+	timestampA = args.slice[0]# - np.min(df.time)
+	timestampB = args.slice[1]# - np.min(df.time)
+
+	while df.iloc[timeIndexA]['time'] > timestampA:
+		timeIndexA += len(df['time']//10000)
+		if timeIndexA >= len(df['time']-1):
+			timeIndexA = len(df['time']-2)
+			break
+	#print('timestampA', timestampA, 'timeIndexA', timeIndexA)
+
+	while df.iloc[timeIndexB]['time'] > timestampB:
+		timeIndexB += len(df['time']//10000)
+		if timeIndexB >= len(df['time']-1):
+			timeIndexB = len(df['time']-1)
+			break
+
+	#print('timestampB', timestampB, 'timeIndexB', timeIndexB)
+
+	# print(timeIndexA)
+	# print(timeIndexB)
+
+	df = df[timeIndexA:timeIndexB]
+
+	df.to_csv('{csvOut}_{start}-{end}_Full.csv'.format(start=args.slice[0], end=args.slice[1], csvOut=csvOut))
+	df[::len(df)//5000].to_csv('{csvOut}_{start}-{end}_005k.csv'.format(start=args.slice[0], end=args.slice[1], csvOut=csvOut))
+	df[::len(df)//15000].to_csv('{csvOut}_{start}-{end}_015k.csv'.format(start=args.slice[0], end=args.slice[1], csvOut=csvOut))
+	df[::len(df)//100000].to_csv('{csvOut}_{start}-{end}_100k.csv'.format(start=args.slice[0], end=args.slice[1], csvOut=csvOut))
+
+else:
+	df.to_csv('{csvOut}_Full.csv'.format(csvOut=csvOut))
+	df[::len(df)//5000].to_csv('{csvOut}_005k.csv'.format(csvOut=csvOut))
+	df[::len(df)//15000].to_csv('{csvOut}_015k.csv'.format(csvOut=csvOut))
+	df[::len(df)//100000].to_csv('{csvOut}_100k.csv'.format(csvOut=csvOut))
+
 print(df)
-
-df.to_csv('{}/FilteredLocationsFull.csv'.format(args.CSV_Out_Dir))
-df[::len(df)//5000].to_csv('{}/FilteredLocations05k.csv'.format(args.CSV_Out_Dir))
-df[::len(df)//15000].to_csv('{}/FilteredLocations15k.csv'.format(args.CSV_Out_Dir))
-
+print('Successfully saved .csv files to {}'.format('/'.join(csvOut.slice('/')[:-1])))
