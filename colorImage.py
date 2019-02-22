1bdeb47eb369700b3b91ad3ffc98145b338ba1a4
Waldinian <waldinian@Alexs-MBP.fios-router.home>
2019-02-22 12:04:24 -0500

    working up through colorImage.py

--- colorImage.py
+++ colorImage.py
@@ -9,14 +9,15 @@ from datetime import datetime
 import sys
 
 print('saving preview image...')
+
 # get csv file
 parser = argparse.ArgumentParser()
-parser.add_argument('fileIn', help='location history CSV file')
-parser.add_argument('fileOut', help='out file path')
+parser.add_argument('csvIn', help='location history CSV file')
+parser.add_argument('pngOut', help='out file path')
 args = parser.parse_args()
 
 # load file in ascending time order, load (n x 1) arrays of lat, lon, and colorbar
-locHist = np.loadtxt(args.fileIn, delimiter = ',', skiprows=1)[::-1]
+locHist = np.loadtxt(args.csvIn, delimiter = ',', skiprows=1)[::-1]
 
 # incredibly messy way of getting an understandable timeline for the colorbar
 dateData = locHist[:,1]
@@ -27,24 +28,14 @@ colorData = ((dateData-dateData[0]))/3.1536E7 + decimalyearstart
 lat = locHist[:,2]
 lon = locHist[:,3]
 
-# creating figure object
 fig = plt.figure(figsize=(120,80))
-
-# for some reason, when we give it an empty array, it spazzes out, so we give it empty slices.
 scat = plt.scatter(lon,lat, c=colorData, s=.4, cmap='viridis_r')
 
-#colorbar and axis limits
-
-#CONUS
+#CONUS, visuals
 plt.xlim(-125,-65)
 plt.ylim(25,50)
-
-#Northeast corridor
-# plt.xlim(-82.,-73.9)
-# plt.ylim(38.,40.83)
-
 plt.clim(colorData[0], colorData[-1])
 plt.axis('off')
 
-plt.savefig(args.fileOut)
+plt.savefig(args.pngOut)
 print('preview image successfully saved')
